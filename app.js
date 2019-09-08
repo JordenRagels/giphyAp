01b24ba254f1471028743044d2dc1ac9e0c4f375
Jorden Ragels <jordenragels3@gmail.com>
2019-09-08 16:52:21 -0700

    Creates an AJAX call for the giphy API

--- app.js
+++ app.js
@@ -6,20 +6,33 @@ var giphyArray = ['koala', 'panda', 'elephant'];
 giphyArray.forEach(function (element) {
     let b = $("<button>");
     b.text(element);
+    b.attr("data-value", element);
     $("#buttons").append(b);
 
 });
 
+$("button").on("click", function () {
+    var searchTerm = $(this).data("value");
 
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=koala&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" 
+    + searchTerm + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
 
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(res){
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (res) {
+        console.log(queryURL);
 
-    console.log(res.data);
+        console.log(res.data);
+
+        var results = res.data;
+
+
+    })
+
+
+
 })
 
 
