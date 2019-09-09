4b1ad906948b1f7ca6b9050bdaa471302d9e2111
Jorden Ragels <jordenragels3@gmail.com>
2019-09-08 18:26:50 -0700

    Changed the state of the gif

--- app.js
+++ app.js
@@ -33,18 +33,35 @@ $("button").on("click", function () {
 
             var resultDiv = $("<div>");
 
+            var rating = results[i].rating;
+
             var gifImage = $("<img>");
+
+            var p = $("<p>").text("Rating: " + rating);
            
             gifImage.attr("src", results[i].images.fixed_height_still.url);
-
+            gifImage.attr("class", "gif");
+            gifImage.attr("data-state", "still");
+            gifImage.attr("data-animate", results[i].images.fixed_height.url);
+            gifImage.attr("data-still", results[i].images.fixed_height_still.url);
             resultDiv.append(gifImage);
+            resultDiv.append(p);
 
             $("#gifResults").prepend(resultDiv);
         }
-
     })
+})
 
-
+$(document).on("click", ".gif", function () {
+    var state = $(this).attr("data-state");
+    
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    } else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
 
 })
 
